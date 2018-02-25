9af50c852448b5888d78f966fae3a1ea1e6c7e13
Fábio Carvalho <fabio.dscar@gmail.com>
2018-02-25 00:14:50 +0000

    Fixed shaders formatting.

--- data/Shaders/unreal.fs
+++ data/Shaders/unreal.fs
@@ -5,9 +5,9 @@
  ============================================================================== */
 // Everything in world coordinates
 in FragData {
-	vec3 position;
-	vec3 normal; 
-	vec2 texCoords;
+    vec3 position;
+    vec3 normal; 
+    vec2 texCoords;
 } vsIn;
 
 /* ==============================================================================
@@ -25,24 +25,24 @@ struct Light {
         Uniforms
  ============================================================================== */
 uniform rendererBlock {
-	float gamma;
-	float exposure;
+    float gamma;
+    float exposure;
 
-	// Uncharted tone map parameters (see common.fs)
-	float A, B, C, D, E, J, W;
+    // Uncharted tone map parameters (see common.fs)
+    float A, B, C, D, E, J, W;
 };
 
 uniform cameraBlock {
-   	mat4 ViewMatrix;
-	mat4 ProjMatrix;
-	mat4 ViewProjMatrix;
-	vec3 ViewPos;
+    mat4 ViewMatrix;
+    mat4 ProjMatrix;
+    mat4 ViewProjMatrix;
+    vec3 ViewPos;
 };
 
 const int NUM_LIGHTS = 4;
 
 uniform lightBlock {
-	Light lights[NUM_LIGHTS];
+    Light lights[NUM_LIGHTS];
 };
 
 // Material parameters
@@ -83,111 +83,111 @@ const float PI = 3.14159265358979;
 out vec4 outColor;
 
 vec3 perturbNormal(in sampler2D normalMap) {
-	// Fetch normal from map and adjust to linear space
-	vec3 normal = texture(normalMap, vsIn.texCoords).xyz * 2.0 - 1.0;
+    // Fetch normal from map and adjust to linear space
+    vec3 normal = texture(normalMap, vsIn.texCoords).xyz * 2.0 - 1.0;
 
-	// Calculate uv derivatives
-	vec2 duvdx = dFdx(vsIn.texCoords);
-	vec2 duvdy = dFdy(vsIn.texCoords);
+    // Calculate uv derivatives
+    vec2 duvdx = dFdx(vsIn.texCoords);
+    vec2 duvdy = dFdy(vsIn.texCoords);
 
-	// Calculate position derivatives
-	vec3 dpdx = dFdx(vsIn.position);
-	vec3 dpdy = dFdy(vsIn.position);
+    // Calculate position derivatives
+    vec3 dpdx = dFdx(vsIn.position);
+    vec3 dpdy = dFdy(vsIn.position);
 
-	// Find tangent from derivates and build the TBN basis
-	vec3 N =  normalize(vsIn.normal);
-	vec3 T =  normalize(dpdx * duvdy.t - dpdy * duvdx.t);
-	vec3 B = -normalize(cross(N, T));
+    // Find tangent from derivates and build the TBN basis
+    vec3 N =  normalize(vsIn.normal);
+    vec3 T =  normalize(dpdx * duvdy.t - dpdy * duvdx.t);
+    vec3 B = -normalize(cross(N, T));
 
-	return normalize(mat3(T, B, N) * normal);
+    return normalize(mat3(T, B, N) * normal);
 }
 
 const float MAX_GGX_LOD = 4.0;
 
 float fetchParameter(sampler2D samp, float val) {
-	if (val >= 0.0)
-		return val;
-	else
-		return texture(samp, vsIn.texCoords).r;
+    if (val >= 0.0)
+        return val;
+    else
+        return texture(samp, vsIn.texCoords).r;
 }
 
 vec3 fetchDiffuse() {
-	if (diffuse.r >= 0)
-		return diffuse;
-	else
-		return toLinearRGB(texture(diffuseTex, vsIn.texCoords).rgb, gamma);
+    if (diffuse.r >= 0)
+        return diffuse;
+    else
+        return toLinearRGB(texture(diffuseTex, vsIn.texCoords).rgb, gamma);
 }
 
 void main(void) {
-	vec3 V = normalize(ViewPos - vsIn.position);
-	vec3 N = perturbNormal(normalTex);
-	vec3 R = reflect(-V, N); 
+    vec3 V = normalize(ViewPos - vsIn.position);
+    vec3 N = perturbNormal(normalTex);
+    vec3 R = reflect(-V, N); 
 
-	float NdotV = max(dot(N, V), 0.0);
+    float NdotV = max(dot(N, V), 0.0);
 
-	float rough = fetchParameter(roughTex, roughness);
-	float metal = fetchParameter(metallicTex, metallic);
+    float rough = fetchParameter(roughTex, roughness);
+    float metal = fetchParameter(metallicTex, metallic);
 
-	/* ==============================================================================
-			Environment
-	============================================================================== */
-	// Diffuse component
-	vec3 kd         = fetchDiffuse();
-	vec3 irradiance = texture(irradianceTex, N).rgb;
-	vec3 diffuse    = kd * irradiance; // Appendix, formula X
+    /* ==============================================================================
+            Environment
+    ============================================================================== */
+    // Diffuse component
+    vec3 kd         = fetchDiffuse();
+    vec3 irradiance = texture(irradianceTex, N).rgb;
+    vec3 diffuse    = kd * irradiance; // Appendix, formula X
 
-	// Specular component
-	vec3 F0 = mix(spec, kd, metal);
-	vec3 F  = fresnelSchlickUnreal(NdotV, spec);
+    // Specular component
+    vec3 F0 = mix(spec, kd, metal);
+    vec3 F  = fresnelSchlickUnreal(NdotV, spec);
 	
-	// Fetch precomputed integrals
-	vec3 prefGGX = textureLod(ggxTex, R, rough * MAX_GGX_LOD).rgb;  
-	vec3 brdf    = texture(brdfTex, vec2(NdotV, rough)).rgb;
+    // Fetch precomputed integrals
+    vec3 prefGGX = textureLod(ggxTex, R, rough * MAX_GGX_LOD).rgb;  
+    vec3 brdf    = texture(brdfTex, vec2(NdotV, rough)).rgb;
 
-	vec3 brdfInt  = F0 * brdf.r + brdf.g; // Appendix, formula Y
-	vec3 specular = prefGGX * brdfInt;    // Appendix, formula Z
+    vec3 brdfInt  = F0 * brdf.r + brdf.g; // Appendix, formula Y
+    vec3 specular = prefGGX * brdfInt;    // Appendix, formula Z
 
-	// Total ambient lighting
-	vec3 retColor = (1.0 - F) * (1.0 - metal) * diffuse + specular;
+    // Total ambient lighting
+    vec3 retColor = (1.0 - F) * (1.0 - metal) * diffuse + specular;
 
 	/* ==============================================================================
 			Lights
 	============================================================================== */
-	vec3 Lrad = vec3(0.0);
-	for(int i = 0; i < NUM_LIGHTS; ++i) {
-		if (!lights[i].state)
-			continue;
+    vec3 Lrad = vec3(0.0);
+    for(int i = 0; i < NUM_LIGHTS; ++i) {
+        if (!lights[i].state)
+            continue;
 
-		vec3 L = normalize(lights[i].position - vsIn.position);
-		vec3 H = normalize(V + L);
+        vec3 L = normalize(lights[i].position - vsIn.position);
+        vec3 H = normalize(V + L);
 
-		float HdotV = max(dot(H, V), 0.0);
-		float NdotL = max(dot(N, L), 0.0);
+        float HdotV = max(dot(H, V), 0.0);
+        float NdotL = max(dot(N, L), 0.0);
 
-		float dist = length(lights[i].position - vsIn.position);
-		vec3 Li    = lights[i].emission / (dist * dist);
+        float dist = length(lights[i].position - vsIn.position);
+        vec3 Li    = lights[i].emission / (dist * dist);
 
-		float fGGX = distGGX(N, H, rough);   
-		float Geo  = geoSmith(N, V, L, rough);    
-		vec3  Fr   = fresnelSchlickUnreal(HdotV, F0);        
+        float fGGX = distGGX(N, H, rough);   
+        float Geo  = geoSmith(N, V, L, rough);    
+        vec3  Fr   = fresnelSchlickUnreal(HdotV, F0);        
         
-		vec3  nom     = fGGX * Geo * Fr;
-		float denom   = 4 * NdotV * NdotL + 0.0001;
-		vec3 contrib  = nom / denom;
+        vec3  nom     = fGGX * Geo * Fr;
+        float denom   = 4 * NdotV * NdotL + 0.0001;
+        vec3 contrib  = nom / denom;
                 
-		// Compute scattering formula for the light
-		Lrad += ((vec3(1.0) - Fr) * (1.0 - metal) * kd / PI + contrib) * Li * NdotL;
-	}   
+        // Compute scattering formula for the light
+        Lrad += ((vec3(1.0) - Fr) * (1.0 - metal) * kd / PI + contrib) * Li * NdotL;
+    }   
 
-	// Sum ambient and lights
-	retColor = retColor + Lrad;
+    // Sum ambient and lights
+    retColor = retColor + Lrad;
 
-	/* ==============================================================================
-		  Post-processing
-	============================================================================== */
-	// Tonemapping and gamma correction
-	retColor = unchartedTonemapParam(retColor, exposure, A, B, C, D, E, J, W);
-	retColor = toInverseGamma(retColor, gamma);
+    /* ==============================================================================
+            Post-processing
+    ============================================================================== */
+    // Tonemapping and gamma correction
+    retColor = unchartedTonemapParam(retColor, exposure, A, B, C, D, E, J, W);
+    retColor = toInverseGamma(retColor, gamma);
 
-	outColor = vec4(retColor, 1.0);
+    outColor = vec4(retColor, 1.0);
 }
--- data/Shaders/unreal.vs
+++ data/Shaders/unreal.vs
@@ -1,7 +1,7 @@
 #version 400
 
 /* ==============================================================================
-		Stage Inputs
+        Stage Inputs
  ============================================================================== */
 layout(location = 0) in vec3 Position;	
 layout(location = 1) in vec3 Normal;
@@ -9,34 +9,34 @@ layout(location = 2) in vec2 TexCoords;
 layout(location = 3) in vec3 Tangent;
 
 /* ==============================================================================
-		Uniforms
+        Uniforms
  ============================================================================== */
 uniform mat4 ModelMatrix;
 uniform mat3 NormalMatrix;
 
 uniform cameraBlock {
-	mat4 ViewMatrix;
-	mat4 ProjMatrix;
-	mat4 ViewProjMatrix;
-	vec3 ViewPos;
+    mat4 ViewMatrix;
+    mat4 ProjMatrix;
+    mat4 ViewProjMatrix;
+    vec3 ViewPos;
 };
 
 /* ==============================================================================
-		Stage Outputs
+        Stage Outputs
 ============================================================================== */
 // Passes everything in world coordinates to the fragment shader
 out FragData {
-	vec3 position;
-	vec3 normal; 
-	vec2 texCoords;
+    vec3 position;
+    vec3 normal; 
+    vec2 texCoords;
 } vsOut;
 
 void main(void) {
-	// Everything in world coordinates
-	vsOut.position  = vec3(ModelMatrix * vec4(Position, 1.0));   
-	vsOut.normal    = normalize(NormalMatrix * Normal);
-	vsOut.texCoords = TexCoords;
+    // Everything in world coordinates
+    vsOut.position  = vec3(ModelMatrix * vec4(Position, 1.0));   
+    vsOut.normal    = normalize(NormalMatrix * Normal);
+    vsOut.texCoords = TexCoords;
 
-	// Return position in MVP coordinates
-	gl_Position = ViewProjMatrix * vec4(vsOut.position, 1.0);
+    // Return position in MVP coordinates
+    gl_Position = ViewProjMatrix * vec4(vsOut.position, 1.0);
 }
